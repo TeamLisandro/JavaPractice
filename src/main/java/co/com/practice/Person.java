8cda911fc712aacafb9fbf911f30e06a43575f9b
Lisandro Betancur <lbetagu@gmail.com>
2022-11-29 08:39:33 -0500

    Classes and objects in Java

---
+++ src/main/java/co/com/practice/Person.java
@@ -0,0 +1,32 @@
+package co.com.practice;
+
+public class Person {
+    private String name;
+    private int age;
+    private String eyeColor;
+    public final boolean HUMAN = true;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setEyeColor(String eyeColor){
+        this.eyeColor = eyeColor;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public String getEyeColor(){
+        return this.eyeColor;
+    }
+}
