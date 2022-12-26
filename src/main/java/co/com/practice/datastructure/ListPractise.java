792cabc722c7d7cd9c55fd39c0a282de8d65105a
Miguel0120 <blancojmg@gmail.com>
2022-12-26 11:51:57 -0500

    creating a List -part 1- ListPractice

--- src/main/java/co/com/practice/datastructure/ListPractise.java
+++ src/main/java/co/com/practice/datastructure/ListPractise.java
@@ -1,4 +1,27 @@
 package co.com.practice.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListPractise {
+    // It is only possible to use non-primitive data inside <>
+    private List<Integer> numberList;
+
+    public ListPractise(){
+        this.numberList=new ArrayList<>();
+    }
+    public void addElements(int element) {
+        this.numberList.add(element);
+    }
+
+    public void addElements(List<Integer> elements) {
+        this.numberList.addAll(elements);
+    }
+
+    public void printElements(){
+        for(Integer index:this.numberList){
+            System.out.println("The number is: " + index);
+        }
+    }
+
 }
